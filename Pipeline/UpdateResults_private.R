804a91ef81fbc9138d5f3540fe25268be953a9ae
Marco Antônio Peixoto <59318360+marcopxt@users.noreply.github.com>
2023-03-04 09:04:32 -0500

    Delete Pipeline directory

--- Pipeline/UpdateResults_private.R
+++
@@ -1,7 +0,0 @@
-#Track performance of parental per se performance
-
-inbredMean_private[year] = meanG(c(F8_private.sel, F7_private.sel))
-inbredVar_private[year] = varG(c(F8_private.sel, F7_private.sel))
-
-
-
